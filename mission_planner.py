c0c3e4aefd9d91382148869a903fcf1c0405a7ac
luqqyme <luqman.azman@gmail.com>
2025-09-23 23:05:53 +0800

    1. changed 4th mission from delivery to maritime patrol 2. mapped drones to respective mission and added description - luqman

--- mission_planner.py
+++ mission_planner.py
@@ -1,9 +1,29 @@
 print("Welcome to Bayraktar Mission Planner")
-print("Choose Mission type: \n 1. Reconnaissance \n 2. Surveillance \n 3. Strike \n 4. Delivery \n")
+print("Choose Mission type: \n 1. Reconnaissance \n 2. Surveillance \n 3. Strike \n 4. Maritime Patrol \n")
 
 mission_type = int(input("Enter Choice: "))
 
 
-
-if mission_type == 2:  #Just an example for the one who is doing this part.. (remove it when u read this)..
-    print("Surveillance Initiated \n Recommended Drone: Bayraktar TB2 \n Endurance: 24 hours \n Purpose: Long-range surveillance and intelligence gathering \n")
+#JY add extra drone descriptions, and format output better - luqman
+if mission_type == 1:  # Mini UAV
+    print("Recommended Drone For Reconnaissance Missions\n\
+          Recommended Drone: Bayraktar Mini UAV\n\
+          Endurance: 60 - 120 minutes\n\
+          Purpose: Short range day and night aerial reconnaissance and surveillance applications\n")
+elif mission_type == 2: # TB2
+    print("Recommended Drone For Surveillance Missions\n\
+          Recommended Drone: Bayraktar TB2\n\
+          Endurance: 27 hours\n\
+          Purpose: Intelligence, surveillance, reconnaissance (ISR), sometimes armed attack missions\n")
+elif mission_type == 3: # Akıncı
+    print("Recommended Drone For Strike Missions\n\
+        Recommended Drone: Bayraktar Akıncı\n\
+        Endurance: 25 hours\n\
+        Purpose: Air-to-ground and air-to-air attack missions by carrying various munitions\n")
+elif mission_type == 4: # Kızılelma
+    print("Recommended Drone For Maritime Patrol Missions\n\
+        Recommended Drone: Bayraktar Kızılelma\n\
+        Endurance: 5-6 hours\n\
+        Purpose: Over-water surveillance, ship escort, anti-surface/anti-submarine scouting and targeting support for naval task groups.\n")
+else: # error handling
+    placeholder = 0  #just placeholder (remove this when do error handling) - luqman
