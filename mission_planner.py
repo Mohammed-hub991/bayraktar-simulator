74819bc17e82465cb7399962584cc3e955579753
Mohammed-hub991 <mohammedsaad7997@gmail.com>
2025-09-23 22:17:48 +0800

    Added Comment..

--- mission_planner.py
+++ mission_planner.py
@@ -5,5 +5,5 @@ mission_type = int(input("Enter Choice: "))
 
 
 
-if mission_type == 2:
+if mission_type == 2:  #Just an example for the one who is doing this part.. (remove it when u read this)..
     print("Surveillance Initiated \n Recommended Drone: Bayraktar TB2 \n Endurance: 24 hours \n Purpose: Long-range surveillance and intelligence gathering \n")
