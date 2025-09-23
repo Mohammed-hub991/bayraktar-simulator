1db1a7c93f54c0ad3b1b5ae37af747ccaef49e0f
Mohammed-hub991 <mohammedsaad7997@gmail.com>
2025-09-23 22:15:19 +0800

    Created mission_planner.py and Added menu system for mission planner, as well as an example for the mission type

---
+++ mission_planner.py
@@ -0,0 +1,9 @@
+print("Welcome to Bayraktar Mission Planner")
+print("Choose Mission type: \n 1. Reconnaissance \n 2. Surveillance \n 3. Strike \n 4. Delivery \n")
+
+mission_type = int(input("Enter Choice: "))
+
+
+
+if mission_type == 2:
+    print("Surveillance Initiated \n Recommended Drone: Bayraktar TB2 \n Endurance: 24 hours \n Purpose: Long-range surveillance and intelligence gathering \n")
